16adb3d739c4617c64759c6e276d8760b6b5d330
deenanath <deenanath.gupta@robosoftin.com>
2016-05-10 14:32:45 +0530

    Pnr Status Work has been done

---
+++ app/src/main/java/indianrail/robosoft/com/indianrail/model/pnrstatus/pnrstatus/ReservationUpto.java
@@ -0,0 +1,14 @@
+package indianrail.robosoft.com.indianrail.model.pnrstatus.pnrstatus;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Author: deena
+ * Created by: ModelGenerator on 10/5/16
+ */
+public class ReservationUpto {
+    @SerializedName("name")
+    public String name;    // KRISHNARAJAPURM
+    @SerializedName("code")
+    public String code;    // KJM  // BAM
+}
